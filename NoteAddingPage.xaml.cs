6c20021f1006fd3ee2584bab7788940ac02fbc84
adel12311 <101188763+adel12311@users.noreply.github.com>
2022-03-11 12:38:59 +0300

    Add files via upload

---
+++ NoteAddingPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using App2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace App2.Views
+{
+    [QueryProperty(nameof(ItemID),nameof(ItemID))]
+    public partial class NoteAddingPage : ContentPage
+    {
+        
+        public string ItemID
+        {
+            set
+            {
+                LoadNote(value);
+            }
+        }
+
+        
+
+        public NoteAddingPage()
+        {
+            InitializeComponent();
+            BindingContext = new Note();
+        }
+
+        private async   void LoadNote(string value)
+        {
+            try
+            {
+                int id = Convert.ToInt32(value);
+
+                Note note = await App.NotesDB.GetApp2Async(id);
+
+                BindingContext = note;
+            }
+            catch { }
+        }
+
+        private async void OnSaveButton_Clicked(object sender, EventArgs e)// вывод и сохранение на экран из БД
+        {
+            Note note = (Note)BindingContext;
+
+            note.Date = DateTime.Now;
+
+
+        
+            
+
+            if (!string.IsNullOrWhiteSpace(note.Text))
+            {
+                await App.NotesDB.SaveNoteAsyns(note);
+            }
+
+
+            await Shell.Current.GoToAsync("..");
+
+            
+
+        }
+
+        private async   void OnDeleteButton_Clicked(object sender, EventArgs e)
+        {
+            Note note = (Note)BindingContext;
+
+            await App.NotesDB.DeleteNoteAsyns(note);
+
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
---
+++ NotesPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App2.Models;
+
+
+
+namespace App2.Views
+{
+    
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+
+
+            collectionView.ItemsSource = await App.NotesDB.GetApp2Async();
+
+            base.OnAppearing();
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(NoteAddingPage));
+        }
+
+        private async void collectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection !=null)
+            {
+                Note note = (Note)e.CurrentSelection.FirstOrDefault();
+                await Shell.Current.GoToAsync(
+                    $"{nameof(NoteAddingPage)}?{nameof(NoteAddingPage.ItemID)}={note.ID.ToString()}");
+            }
+        }
+
+        private async void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+     
+           }
+        }
+    }

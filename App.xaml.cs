9963c4e6b12d335f01b50c847e04c08425a283f9
adel12311 <101188763+adel12311@users.noreply.github.com>
2022-03-11 12:35:38 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App2.Data;
+using System.IO;
+//using Com.OneSignal;
+
+namespace App2
+{
+    public partial class App : Application
+    {
+        static NotesDB notesDB;
+
+        public static NotesDB NotesDB
+
+        {
+            get 
+            { 
+                if(notesDB == null)
+                {
+                    notesDB = new NotesDB(
+                        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), 
+                        "NotesDataBase.db3"));
+                        
+                }
+                return notesDB;
+            }
+        }
+
+
+
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+//OneSignal.Current.StartInit("Не забудь сделать зарядку")
+        //        .EndInit();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using App2.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+   
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(NoteAddingPage), typeof(NoteAddingPage));
+        }
+    }
+}
